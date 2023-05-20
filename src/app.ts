ea9add757ed1e65200507303bb2a46d9df59eeef
João Augusto Zanardo de Lima <augustozanardo@yahoo.com.br>
2023-05-19 21:37:00 -0300

    chore: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'João Zanardo',
+    email: 'jozanardo@gmail.com',
+  },
+})
