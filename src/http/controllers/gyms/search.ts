a56deeac2c87e446d3dbac5abcb6fa2403407e27
João Augusto <62316215+jozanardo@users.noreply.github.com>
2024-02-08 00:06:01 -0300

    feat: create routes for gyms

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(201).send({ message: 'Usuário criado com sucesso', gyms })
+}
